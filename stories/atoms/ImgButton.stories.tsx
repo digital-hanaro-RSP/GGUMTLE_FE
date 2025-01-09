592213c15e7a553e5d1527ac4b6e8d7cec3e92d9
Sung Yeob Jung <dev.sungyeob@gmail.com>
2025-01-09 17:17:05 +0900

    [fix] 스토리북 일부 수정

--- stories/atoms/ImgButton.stories.tsx
+++ stories/atoms/ImgButton.stories.tsx
@@ -2,6 +2,7 @@ import { ImgButton } from '@/components/atoms/Button';
 import type { Meta, StoryObj } from '@storybook/react';
 
 const meta: Meta<typeof ImgButton> = {
+  tags: ['autodocs'],
   title: 'Atoms/ImgButton',
   component: ImgButton,
 };
@@ -10,17 +11,17 @@ export default meta;
 
 type Story = StoryObj<typeof ImgButton>;
 
-export const DefaultImageButton: Story = {
-  args: {
-    src: 'https://picsum.photos/100/100',
-    size: 'md',
-  },
-};
-
 export const SmallImageButton: Story = {
   args: {
     src: 'https://picsum.photos/100/100',
     size: 'sm',
+  },
+};
+
+export const DefaultImageButton: Story = {
+  args: {
+    src: 'https://picsum.photos/100/100',
+    size: 'md',
   },
 };
 
