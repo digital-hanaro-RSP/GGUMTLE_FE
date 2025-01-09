b4d54e01d89afd0e726c6bd8862dca69503d7f5b
99andrew99 <whalstjr199927@gmail.com>
2025-01-09 15:54:48 +0900

    [feat] input atom component 구현

--- components/atoms/Inputs.tsx
+++ components/atoms/Inputs.tsx
@@ -1,3 +1,6 @@
+'use client';
+
+import { IoSearch } from 'react-icons/io5';
 import { TiDelete } from 'react-icons/ti';
 import {
   ChangeEvent,
@@ -102,4 +105,102 @@ function DefaultInput(
 }
 const DefaultInputRef = forwardRef(DefaultInput);
 
-export { DefaultInputRef };
+/** ------------------------------------------ */
+type MoneyInputProps = {
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  onFocus?: () => void;
+  onBlur?: (e: React.FocusEvent<HTMLInputElement>) => void;
+  placeHolder?: string;
+};
+
+const MoneyInputRef = forwardRef<HTMLInputElement, MoneyInputProps>(
+  ({ onChange, onFocus, onBlur, placeHolder }, ref) => {
+    const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+      const inputValue = e.target.value.replace(/[^\d]/g, '');
+      const numericValue = parseInt(inputValue, 10);
+
+      if (!isNaN(numericValue) && numericValue <= Number.MAX_SAFE_INTEGER) {
+        const formattedValue = new Intl.NumberFormat('ko-KR').format(
+          numericValue
+        );
+        if (ref && 'current' in ref && ref.current) {
+          ref.current.value = formattedValue;
+        }
+      } else {
+        if (ref && 'current' in ref && ref.current) {
+          ref.current.value = ref.current.value.slice(0, -1);
+        }
+      }
+      onChange(e);
+    };
+
+    return (
+      <div className='flex w-full text-2xl text-primary-main'>
+        <input
+          type='text'
+          ref={ref}
+          onChange={handleChange}
+          onFocus={onFocus}
+          onBlur={onBlur}
+          placeholder={placeHolder}
+          max={Number.MAX_VALUE}
+          className='flex w-full text-center text-2xl font-semibold placeholder:text-primary-placeholder '
+        />
+        {ref &&
+          'current' in ref &&
+          ref.current &&
+          parseInt(ref.current.value, 10) > 0 && <span>원</span>}
+      </div>
+    );
+  }
+);
+
+MoneyInputRef.displayName = 'MoneyInputRef';
+
+/** ------------------------------------------ */
+type SearchInputProps = baseInputTypeProps & {
+  name?: string;
+  onSubmit: (e: React.FormEvent<HTMLFormElement>) => void;
+  value?: string;
+};
+
+/**onSubmit에 따라 검색을 진행합니다. */
+function SearchInput(
+  { name, placeHolder, className, onSubmit, ...props }: SearchInputProps,
+  ref: ForwardedRef<HTMLInputElement>
+) {
+  const SearchHandler = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    onSubmit(e);
+    console.log('button click');
+  };
+
+  return (
+    <>
+      <form onSubmit={SearchHandler} className='input-box'>
+        <input
+          className={cn(
+            'border border-hanaPrimary px-4 rounded-xl h-11 text-hanaPrimary',
+
+            className
+          )}
+          name={name}
+          placeholder={placeHolder}
+          ref={ref}
+          {...props}
+        />
+        <div className='absolute h-11 w-fit right-0 flex z-100'>
+          <button
+            type='submit'
+            className='absolute right-4 top-1/2 transform -translate-y-1/2 focus:outline-none cursor-pointer'
+          >
+            <IoSearch size={20} />
+          </button>
+        </div>
+      </form>
+    </>
+  );
+}
+const SearchInpuRef = forwardRef(SearchInput);
+
+export { DefaultInputRef, MoneyInputRef, SearchInpuRef };
