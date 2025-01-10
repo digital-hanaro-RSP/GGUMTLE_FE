0ac1e47c6c07819ae6c1068a4a1c1946210d5f87
awaySH <gkgkjs@gmail.com>
2025-01-10 11:22:34 +0900

    [fix] 프로필 이미지 컴포넌트 반응형 고려

--- components/atoms/UserProfile.tsx
+++ components/atoms/UserProfile.tsx
@@ -11,14 +11,17 @@ const UserProfile: ForwardRefRenderFunction<
   UserProfileProps
 > = ({ imageUrl, alt = 'User profile' }, ref) => {
   return (
-    <div ref={ref} className='relative w-[36px] h-[36px]'>
+    <div
+      ref={ref}
+      className='relative w-[36px] h-[36px] sm:w-[42px] sm:h-[42px] md:w-[48px] md:h-[48px] lg:w-[54px] lg:h-[54px]'
+    >
       <Image
         src={imageUrl}
         alt={alt}
         width={36}
         height={36}
-        className='rounded-full object-cover'
-        style={{ width: '100%', height: '100%' }}
+        className='rounded-full object-cover w-full h-full'
+        priority
       />
     </div>
   );
