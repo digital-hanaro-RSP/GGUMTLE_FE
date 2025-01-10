8e55a1ea27953507ce35807042a221151e865fc3
awaySH <gkgkjs@gmail.com>
2025-01-10 11:29:06 +0900

    [fix] show selected image 컴포넌트 반응형 고려

--- components/atoms/ShowSelectedImage.tsx
+++ components/atoms/ShowSelectedImage.tsx
@@ -27,14 +27,14 @@ const ShowSelectedImage: ForwardRefRenderFunction<
   }));
 
   return (
-    <div className='relative w-[100px] h-[100px]'>
+    <div className='relative w-[100px] h-[100px] sm:w-[120px] sm:h-[120px] md:w-[140px] md:h-[140px] lg:w-[160px] lg:h-[160px]'>
       <Image
         src={imageUrl}
         alt='Selected'
         width={100}
         height={100}
-        className='object-cover rounded-[10px]'
-        style={{ width: '100%', height: '100%' }}
+        className='object-cover rounded-[10px] w-full h-full'
+        priority
       />
       <button
         onClick={() => {
@@ -42,7 +42,7 @@ const ShowSelectedImage: ForwardRefRenderFunction<
             onRemove();
           }
         }}
-        className='absolute -top-2 -right-2 w-6 h-6 bg-primary-main text-white rounded-full flex items-center justify-center hover:bg-primary-hover z-10 text-2xl leading-none pb-1'
+        className='absolute -top-2 -right-2 w-6 h-6 sm:w-7 sm:h-7 md:w-8 md:h-8 lg:w-9 lg:h-9 bg-primary-main text-white rounded-full flex items-center justify-center hover:bg-primary-hover z-10 text-2xl leading-none pb-1'
       >
         -
       </button>
