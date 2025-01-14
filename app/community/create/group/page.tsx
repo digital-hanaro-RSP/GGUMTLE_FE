c814538b7f29b65f65604f61893e718ae5ece784
99andrew99 <whalstjr199927@gmail.com>
2025-01-14 16:54:49 +0900

    [feat] 중간 정검

--- app/community/create/group/page.tsx
+++ app/community/create/group/page.tsx
@@ -1,3 +1,18 @@
+import Header from '@/components/atoms/Header';
+import { DefaultInputRef } from '@/components/atoms/Inputs';
+
 export default function CreateGroupPage() {
-  return <div>CreateGroupPage</div>;
+  return (
+    <div className='flex flex-col gap-[20px] w-full'>
+      <Header text='꿈모임 만들기' showActionButton={false} />
+      <div className='flex flex-col gap-[20px] w-full px-[20px]'>
+        <div className='flex flex-col gap-[7px]'>
+          <p className='text-[18px] font-bold'>
+            생성할 꿈모임의 이름을 입력해주세요.
+          </p>
+          <DefaultInputRef placeHolder='꿈모임 이름을 입력해주세요.' />
+        </div>
+      </div>
+    </div>
+  );
 }
