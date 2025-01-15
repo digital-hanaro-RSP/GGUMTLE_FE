95168ee15191382239d4ec4dda42c1f3b5540273
awaySH <gkgkjs@gmail.com>
2025-01-15 16:02:28 +0900

    [fix] 페이지 전체 레이아웃 수정

--- app/(auth)/start/page.tsx
+++ app/(auth)/start/page.tsx
@@ -9,7 +9,7 @@ export default function StartPage() {
   const router = useRouter();
 
   return (
-    <div className='fixed inset-0 flex flex-col items-center justify-center p-4 gap-40'>
+    <div className='fixed inset-0 flex flex-col items-center justify-center p-4 gap-32'>
       {/* 상단 컨텐츠 그룹 */}
       <div className='flex flex-col items-center gap-4'>
         {/* 이름 섹션 */}
@@ -27,7 +27,7 @@ export default function StartPage() {
         </p>
 
         {/* GIF 섹션 */}
-        <div className='relative w-60 h-60 translate-y-32'>
+        <div className='relative w-60 h-60 translate-y-20'>
           <Image
             src='/gif/img8.gif'
             alt='Start page animation'
