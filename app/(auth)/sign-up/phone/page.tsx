849b3b706cb5bf5e53788be8ab6eedb1963192d4
awaySH <gkgkjs@gmail.com>
2025-01-15 23:14:40 +0900

    [feat] 전화번호 인증 페이지 구현 완료

--- app/(auth)/sign-up/phone/page.tsx
+++ app/(auth)/sign-up/phone/page.tsx
@@ -2,13 +2,38 @@
 
 import { Button } from '@/components/atoms/Button';
 import { DefaultInputRef } from '@/components/atoms/Inputs';
-import { LuListCheck } from 'react-icons/lu';
+import { useSignUpStore } from '@/store/useSignUpStore';
+import { RiPhoneFindLine } from 'react-icons/ri';
+import router from 'next/router';
 import { useState, useEffect } from 'react';
 
 export default function PhonePage() {
   const [showVerification, setShowVerification] = useState(false);
   const [timeLeft, setTimeLeft] = useState(180); // 3 minutes in seconds
   const [timerExpired, setTimerExpired] = useState(false);
+  const setPhoneInfo = useSignUpStore((state) => state.setPhoneInfo);
+
+  const formatPhoneNumber = (phoneNumber: string) => {
+    const cleaned = phoneNumber.replace(/\D/g, '');
+    const match = cleaned.match(/^(\d{3})(\d{4})(\d{4})$/);
+    if (match) {
+      return `${match[1]}-${match[2]}-${match[3]}`;
+    }
+    return phoneNumber;
+  };
+
+  const handleVerificationSuccess = () => {
+    const phoneInput = document.querySelector(
+      "input[name='phone']"
+    ) as HTMLInputElement;
+    if (phoneInput) {
+      const formattedPhone = formatPhoneNumber(phoneInput.value);
+      setPhoneInfo(formattedPhone);
+      console.log('저장된 정보: ', useSignUpStore.getState().formData);
+      alert('인증에 성공하였습니다.');
+      router.push('/sign-up/profile');
+    }
+  };
 
   useEffect(() => {
     let timer: NodeJS.Timeout;
@@ -45,14 +70,14 @@ export default function PhonePage() {
 
   return (
     <div className='p-4'>
-      <div className='flex flex-col items-center gap-10'>
+      <div className='flex flex-col items-center gap-14'>
         <div className='flex flex-col items-center mt-3'>
-          <LuListCheck className='text-primary-main text-5xl mb-3' />
+          <RiPhoneFindLine className='text-primary-main text-5xl mb-3' />
           <h1 className='text-xl font-bold tracking-tighter whitespace-pre-line text-center text-primary-main mb-4'>
-            아래 내용을 하나씩{'\n'}차근차근 입력해주세요
+            간단한 휴대폰 인증으로{'\n'}본인 확인을 완료하세요
           </h1>
         </div>
-        <div className='w-full max-w-md space-y-6'>
+        <div className='w-full max-w-md space-y-10'>
           <div>
             <p className='text-lg font-bold mb-2 text-[#5B5B5B]'>휴대폰 인증</p>
             <div className='flex flex-row gap-2'>
@@ -75,44 +100,43 @@ export default function PhonePage() {
           </div>
 
           {showVerification && (
-            <>
-              <div>
-                <div className='text-primary-main font-medium'>
-                  <span className='flex justify-center'>
-                    전화번호로 인증번호를 보내드렸습니다.
-                  </span>
-                  <span className='flex justify-center'>
-                    3분 안에 입력해 주세요.
-                  </span>
+            <div className='animate-fadeIn space-y-10'>
+              <div className='text-primary-main font-medium'>
+                <span className='flex justify-center'>
+                  전화번호로 인증번호를 보내드렸습니다.
+                </span>
+                <span className='flex justify-center'>
+                  3분 안에 입력해 주세요.
+                </span>
+              </div>
+
+              <div className='space-y-4'>
+                <DefaultInputRef
+                  name='name'
+                  placeHolder='인증번호 입력해주세요'
+                  required
+                  error='인증번호 입력해주세요'
+                />
+
+                <div className='flex justify-center'>
+                  {!timerExpired ? (
+                    <div className='text-primary-main'>
+                      유효시간 {formatTime(timeLeft)}
+                    </div>
+                  ) : (
+                    <div className='text-primary-main'>
+                      유효시간이 모두 끝났습니다.
+                    </div>
+                  )}
                 </div>
               </div>
-              {/* <div className='w-10 h-10 mx-auto flex items-center justify-center'>
-                <Image
-                  src='/image/icons/hourglass.png'
-                  alt={'hourglass icon'}
-                  width={40}
-                  height={40}
-                />
-              </div> */}
-              <DefaultInputRef
-                name='name'
-                placeHolder='인증번호 입력해주세요'
-                required
-                error='인증번호 입력해주세요'
-              />
 
-              <div className='flex justify-center'>
-                {!timerExpired ? (
-                  <div className=' text-primary-main'>
-                    유효시간 {formatTime(timeLeft)}
-                  </div>
-                ) : (
-                  <div className=' text-primary-main'>
-                    유효시간이 모두 끝났습니다.
-                  </div>
-                )}
+              <div className='flex flex-col items-center'>
+                <Button size='lg' onClick={handleVerificationSuccess}>
+                  인증번호 확인
+                </Button>
               </div>
-            </>
+            </div>
           )}
         </div>
       </div>
