54eb688cf9da87ec53f645b304ae850af2df6ca8
Sung Yeob Jung <dev.sungyeob@gmail.com>
2025-01-29 17:26:22 +0900

    [feat] progressbar mention

--- components/molecules/ProgressBar.tsx
+++ components/molecules/ProgressBar.tsx
@@ -35,6 +35,15 @@ export const ProgressBar = ({ dataPercent, className }: ProgressBarProps) => {
     return () => clearInterval(timer);
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [showPercent]);
+
+  const mention = () => {
+    if (dataPercent < 10) return '이제 시작이네요. 화이팅!';
+    if (dataPercent < 40) return '오 대단해요!';
+    if (dataPercent < 70) return '벌써 절반 가까이 왔어요!';
+    if (dataPercent < 99) return '거의 다 왔어요!';
+    return '목표를 달성했어요!';
+  };
+
   return (
     <div className='w-full flex flex-col justify-center items-center'>
       <div className={cn('relative pt-10 pb-2 w-11/12', className)}>
@@ -57,6 +66,20 @@ export const ProgressBar = ({ dataPercent, className }: ProgressBarProps) => {
             )}
           >
             {countProgress.toFixed(0)}%
+          </div>
+        </span>
+        <span
+          className={cn('absolute -top-1 w-full transition duration-1000')}
+          style={{ transform: `translateX(-${100 - (progress || 0)}%)` }}
+        >
+          <div
+            className={cn(
+              'text-base break-keep -top-[30px] absolute w-36 transition duration-1000 font-semibold text-primary-main bg-transparent px-1 rounded-md backdrop-blur-lg bg-opacity-30 floating border-primary-main border-2 text-center py-2 ',
+              showPercent ? 'opacity-100' : 'opacity-0',
+              dataPercent < 60 ? 'left-[325px] rounded-bl-none' : 'right-12 rounded-br-none'
+            )}
+          >
+            {mention()}
           </div>
         </span>
       </div>
