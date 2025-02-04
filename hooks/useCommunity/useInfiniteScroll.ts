8b6c065895b42aee2e4be5ed736023aa217c478b
99andrew99 <whalstjr199927@gmail.com>
2025-02-04 09:35:06 +0900

    [refactor] useInfiniteScroll 타입 수정

--- hooks/useCommunity/useInfiniteScroll.ts
+++ hooks/useCommunity/useInfiniteScroll.ts
@@ -8,8 +8,7 @@ interface UseInfiniteScrollProps<T> {
     search?: string;
   }) => Promise<T[]>;
   limit?: number;
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  dependencies?: any[];
+  dependencies?: string[];
   category?: string;
   search?: string;
 }
